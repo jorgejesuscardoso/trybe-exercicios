933fc6dd722a00322e79787b6e56d440bbef963c
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-05-31 18:00:15 -0300

    Exemplo CSGO

--- 01-fundamentos/secao-03-introducao-a-JavaScript/dia-01-Primeiros-passos-no-JavaScript/cs-go.js
+++ 01-fundamentos/secao-03-introducao-a-JavaScript/dia-01-Primeiros-passos-no-JavaScript/cs-go.js
@@ -1,6 +1,6 @@
 const round = 1; // entree 1-16
 const killweapon = 'knife'; // 'AWP', 'Molotov', 'Desert Eagle', 'MP7'
-const howin = 'Bomb Explode'; // 'Eliminate', 'Time out', 'Bomb Has Been Planted', 'Bomb has been Defused'
+const howin = 'Bomb Explode'; // 'Eliminate', 'Time Out', 'Bomb Has Been Planted', 'Bomb has been Defused'
 const win = true;
 const bombDetonate = false;
 const defusedBomb = true;
@@ -10,6 +10,10 @@ let money = 0;
 if (round === 1 || round === 16){
 
     money = 800;
+
+} else if ( win === true && (howin === 'Eliminate' || howin === 'Time Out')){
+
+    money += 3250;
 
 }
 
