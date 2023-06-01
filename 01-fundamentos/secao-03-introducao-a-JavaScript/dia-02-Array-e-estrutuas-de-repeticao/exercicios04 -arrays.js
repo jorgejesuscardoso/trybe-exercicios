318695f7c0cbb4b77f3176784488019808e8e375
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-06-01 16:25:11 -0300

    arrays

---
+++ 01-fundamentos/secao-03-introducao-a-JavaScript/dia-02-Array-e-estrutuas-de-repeticao/exercicios04 -arrays.js
@@ -0,0 +1,19 @@
+/*Método 	        Descrição
+
+push()-------------Adiciona um elemento ao final do array
+unshift()----------Adiciona um elemento no início do array
+pop()--------------Remove o último elemento do array
+shift()------------Remove o primeiro elemento do array */
+
+
+let shoppingList = [];
+
+
+shoppingList.push('Leite');
+shoppingList.push('Contonetes');
+shoppingList.unshift('Água de coco');
+shoppingList.pop();
+shoppingList.shift();
+
+
+console.log(shoppingList);
