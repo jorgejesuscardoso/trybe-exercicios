3a2d43c86d13a1bc376ef01b1b7464a79becdd29
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-06-01 13:20:41 -0300

    Inicio monitoria

---
+++ 01-fundamentos/secao-03-introducao-a-JavaScript/dia-02-Array-e-estrutuas-de-repeticao/monitoria.js
@@ -0,0 +1,20 @@
+let age = 24;
+
+let firstName = 'Ana';
+
+let havePet = true;
+
+console.log(typeof havePet);
+
+if(havePet === true){
+    console.log('Possui animal de estimação!');
+} else{
+    
+    console.log('Não possui animal de estimação!')
+}
+
+if(firstName === 'Ana' && havePet === true){
+    console.log('Esse pet pertence a Ana');
+} else {
+    console.log('Esse pet pertence a Ana');
+}
