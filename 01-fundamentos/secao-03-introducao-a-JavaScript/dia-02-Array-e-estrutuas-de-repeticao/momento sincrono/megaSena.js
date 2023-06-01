42bffb717fbb381837fae99cfa365186d65c616a
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-06-01 17:29:24 -0300

    mega sena part 1

---
+++ 01-fundamentos/secao-03-introducao-a-JavaScript/dia-02-Array-e-estrutuas-de-repeticao/momento sincrono/megaSena.js
@@ -0,0 +1,15 @@
+// Teste inicial
+
+const numerosSorteados = [];
+const meuJogo = [5,13,24,35,48,57];
+
+const numeroAleatorio = Math.floor(Math.random()*60);
+
+for (let repetecao = 0; repetecao < 6; repetecao += 1){
+
+    const numeroAleatorio = Math.floor(Math.random()*60);
+
+    console.log(numeroAleatorio);
+
+}
+
--- 01-fundamentos/secao-03-introducao-a-JavaScript/dia-02-Array-e-estrutuas-de-repeticao/momento sincrono/momento-sincrono-Array-Loop.js
+++
@@ -1,8 +0,0 @@
-// Mega Sena
-
-const numeros = [1,2,3,4,5,6]
-
-for (let index = 0; index < numeros.length; index += 1){
-
-console.log(numeros[index]);
-}
