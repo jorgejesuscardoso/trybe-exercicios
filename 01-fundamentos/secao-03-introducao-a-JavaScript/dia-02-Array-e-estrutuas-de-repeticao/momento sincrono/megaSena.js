10a548312a72fc230f936b0e45cdd8df47a8e6ee
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-06-01 18:07:02 -0300

    Mega Sena

--- 01-fundamentos/secao-03-introducao-a-JavaScript/dia-02-Array-e-estrutuas-de-repeticao/momento sincrono/megaSena.js
+++ 01-fundamentos/secao-03-introducao-a-JavaScript/dia-02-Array-e-estrutuas-de-repeticao/momento sincrono/megaSena.js
@@ -1,15 +1,35 @@
 // Teste inicial
 
 const numerosSorteados = [];
-const meuJogo = [5,13,24,35,48,57];
-
-const numeroAleatorio = Math.floor(Math.random()*60);
+const meuJogo = [8,30,9,83,32,56];
 
 for (let repetecao = 0; repetecao < 6; repetecao += 1){
 
     const numeroAleatorio = Math.floor(Math.random()*60);
 
-    console.log(numeroAleatorio);
+    if (!numerosSorteados.includes(numeroAleatorio)){
+    numerosSorteados.push(numeroAleatorio) 
 
+    } else{
+        repetecao -= 1;
+    }
 }
+
+let numrosAcertei = [];
+let numerosAcertos = 0;
+
+for (let indexSorteado = 0; indexSorteado < numerosSorteados.length; indexSorteado += 1){
+  for (let indexMeuJogo = 0; indexMeuJogo < meuJogo.length; indexMeuJogo += 1){
+        if (numerosSorteados[indexSorteado] === meuJogo[indexMeuJogo]){
+            numerosAcertos += 1;
+            numrosAcertei.push(numerosSorteados[indexSorteado]);
+        }
+
+    }
+}
+
+console.log(`Meu jogo: ${meuJogo}`);
+console.log(`Numeros sorteados: ${numerosSorteados}`);
+console.log(`Numeros de Acertos: ${numerosAcertos}`);
+console.log(`Numeros que acertei: ${numrosAcertei}`)
 
