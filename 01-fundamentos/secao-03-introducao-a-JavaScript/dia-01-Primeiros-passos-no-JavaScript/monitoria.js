72fa038fb974c289b938e2033cf5f60c610d1c6e
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-06-03 15:03:32 -0300

    fim da mentoria

---
+++ 01-fundamentos/secao-03-introducao-a-JavaScript/dia-01-Primeiros-passos-no-JavaScript/monitoria.js
@@ -0,0 +1,16 @@
+// Intro switch case
+
+const ticket = "2023NT3IRA3";
+let message = '';
+
+switch(ticket){
+    case '2023ME14':
+        message = 'Meia: Dirija-se a até a fila A';
+        break;
+        case '2023NT3IRA':
+            message = 'Inteira: DIrija-se até a fila B';
+            break;
+            default:
+                message = 'Código inválido';
+                break;
+}
