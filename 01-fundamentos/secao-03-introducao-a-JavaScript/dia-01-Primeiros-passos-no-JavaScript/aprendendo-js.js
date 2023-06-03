db262c581a84669432033df33ad8caf60d8d563a
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-06-03 12:38:02 -0300

    att

--- 01-fundamentos/secao-03-introducao-a-JavaScript/dia-01-Primeiros-passos-no-JavaScript/aprendendo-js.js
+++ 01-fundamentos/secao-03-introducao-a-JavaScript/dia-01-Primeiros-passos-no-JavaScript/aprendendo-js.js
@@ -1,8 +1,9 @@
 //const nomePessoa= 'Jorge';
 
-    
+//  tipos primitivos  -  tipagem dinamica
+//  string (''), number (01), boolean (true/false), undefined (nao definido), functio (função), object (null ou objeto)
 
-
+/*
 if (true){
 
     var xablau = 'Vaza o scopo';
@@ -10,3 +11,35 @@ if (true){
 }
  
 console.log(xablau);
+
+*/
+/*
+
+//  operadores:
+
+//  atribuição =
+let comparacao = 3;
+let comparacao1 = "3";
+//  aritiméticos + - * / ** %
+console.log(3**2);
+console.log(3/2);
+console.log(3+2);
+console.log(3-2);
+console.log(3*2);
+console.log(3%2);
+//  comparação == === > < >= <=
+
+console.log(comparacao == comparacao1); //  vẽ se o valor é igual
+console.log(comparacao === comparacao1); // vẽ se o valor é estritamente igual, tanto o valor quanto a tipagem,
+*/
+//  Operadores lógico
+//  usados com valores booleanos (lógicos)
+
+// && significa AND (exemplo, isso 'e' aquilo) precisa que ambas sejam iguais
+// || significa OR (exemplo, 'ou' isso 'ou' aquilo) precisa que pelomenos uma seja true
+// ! NOT,significa negação, inverte o valor booleano
+//exemplo.
+
+let negacao = true;
+
+console.log(!negacao);
