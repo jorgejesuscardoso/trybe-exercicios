a457bf96e9a943c338d66c0a93bc558df1588bb5
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-06-05 16:38:39 -0300

    acessando chaves no objeto

---
+++ 01-fundamentos/secao-03-introducao-a-JavaScript/dia-04-objetos/mcDonalds.js
@@ -0,0 +1,14 @@
+const mcDonalds = {
+    bigMac: 45.90,
+    bigTasty: 39.99,
+    mcFritasMedia: 9.90,
+    city: 'Curitiba',
+    unit: ['Shopping de Curitiba','Shopping Estação'],
+    isDelcicius: true,
+};
+
+//console.log(mcDonalds.bigTasty);
+
+//console.log(mcDonalds['bigMac']);
+const chave = 'bigMac';
+console.log(mcDonalds[chave]);
