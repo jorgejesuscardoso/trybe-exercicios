4dea81abcf9bc6124ae77a4e00e3766c6d55ac35
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-06-05 15:39:36 -0300

    att

---
+++ 01-fundamentos/secao-03-introducao-a-JavaScript/dia-02-Array-e-estrutuas-de-repeticao/lsitaDeCompras.js
@@ -0,0 +1,11 @@
+const shopping = ['Sabão em pó','Macarrão','Leite condensado','Refrigerante','Maçã','Alface','Nescau'];
+
+for(let index = 0;index < shopping.length; index += 1){
+
+   // console.log(index);
+
+   //console.log(shopping[index]);
+
+   console.log(`${index} - ${shopping[index]}`);
+
+}
