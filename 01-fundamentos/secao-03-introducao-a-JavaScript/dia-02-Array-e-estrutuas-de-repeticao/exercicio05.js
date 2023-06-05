0200307409fa8a8c962702b2039a1d11e9f24ffe
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-06-05 16:25:53 -0300

    exercicios

---
+++ 01-fundamentos/secao-03-introducao-a-JavaScript/dia-02-Array-e-estrutuas-de-repeticao/exercicio05.js
@@ -0,0 +1,74 @@
+// Exercicio 1:
+// Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. É necessário que seu código tenha duas variáveis, num1 e num2, definidas no começo com os valores que serão operados. Escreva códigos para:
+
+/*let num1 = 11; 
+let num2 = 23;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+*/
+
+
+// Exercicio 2:
+// Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três variáveis com os valores que serão comparados.
+
+
+/*
+let num1 = 72;
+let num2 = 55;
+let num3 = 84;
+
+if(num1 > num2 && num1 > num3){
+
+    console.log(`O maior número é o: ${num1}!`);
+
+} else if (num2 > num1 && num2 > num3){
+
+    console.log(`O maior número é o: ${num2}!`);
+
+}else{
+
+    console.log(`O maio número é o: ${num3}!`);
+
+}*/
+
+// Exercicio 3:
+// Utilize switch/case para escrever um código que receba o nome de uma peça de xadrez e retorne os movimentos que ela pode fazer.
+
+//Se a peça passada for inválida, o código deve retornar uma mensagem de erro.
+
+//Desafio extra, escreva um código para funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais. Uma dica é pesquisar uma função que faça uma string ficar com todas as letras minúsculas (lower case).
+
+//Exemplo: Bispo -> Diagonais.
+/*
+switch (Bispo) {
+    case Bispo:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+
+// Exercicio 4:
+// Utilize if...else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, o código deve retornar false.
+//Faça esse exercício utilizando somente um if.
+/*
+const num1 = 3;
+const num2 = 7;
+const num3 = 6;
+
+if(num1%2 == 0 || num2%2 == 0 || num3%2 == 0){
+
+    console.log('True');
+
+} else{
+    console.log('False');
+}*/
+
+// Exercicio 5:
+//
