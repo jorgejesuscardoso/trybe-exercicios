b44dab87087cd00c369b766562f7a060307d89d9
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-06-05 17:06:25 -0300

    menu

---
+++ 01-fundamentos/secao-03-introducao-a-JavaScript/dia-04-objetos/menu.js
@@ -0,0 +1,20 @@
+const menu = {
+    sandwiches: [
+        {
+            name: 'Big Mac',
+            price: 30.99,
+            ingredientes: ['2 hamburgueres','Alface americana','queijo cheddar','molho especial'],
+            calories: 500,
+        }
+    ],
+    dessert: [
+        {
+            name: 'Big Mac',
+            price: 30.99,
+            ingredientes: ['2 hamburgueres','Alface americana','queijo cheddar','molho especial'],
+            calories: 500,
+        }
+    ],
+    drinks: [],
+    sideDishes:[],
+};
