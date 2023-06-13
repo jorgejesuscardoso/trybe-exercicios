24df13322784e9923fd2b393d6ebbac4ade11868
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-06-13 12:14:56 -0300

    DOM

---
+++ 01-fundamentos/secao-04-JavaScript-DOM-Eventos-e-Web-Storage/Dia-01-JavaScript-DOM-e-seletores/js/getElementsByClassName.js
@@ -0,0 +1,8 @@
+let pilotosF1Atual = document.getElementsByClassName('piloto-f1-atual');
+
+for (let index = 0; index < pilotosF1Atual.length; index += 1){
+
+    pilotosF1Atual[index].innerText = 'Lewis Hamilton'
+
+
+}
