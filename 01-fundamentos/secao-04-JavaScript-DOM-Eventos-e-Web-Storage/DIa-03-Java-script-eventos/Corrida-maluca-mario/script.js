d0117f72973835f478d46ea3badd3ae943ad742e
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-06-16 09:36:13 -0300

    feat: refazendo o joguinho

--- 01-fundamentos/secao-04-JavaScript-DOM-Eventos-e-Web-Storage/DIa-03-Java-script-eventos/Corrida-maluca-mario/script.js
+++ 01-fundamentos/secao-04-JavaScript-DOM-Eventos-e-Web-Storage/DIa-03-Java-script-eventos/Corrida-maluca-mario/script.js
@@ -10,56 +10,13 @@ window.onload = () => {
     const player2 = document.getElementById('player2');         // captura o elemento player2
     const audioWinner = document.getElementById('audioWinner'); // captura o elemento de audio
 
-    player1.style.marginLeft = 0;
-    player2.style.marginRight = 0;
+player1.style.marginLeft = 0;
+player1.style.marginLeft = 0;
 
-    startBtn.addEventListener( 'click' , () => {
-        player1.style.marginLeft = (parseInt(player1.style.marginLeft)) + gerarNumeroAleatorio() + (px)});
+startBtn.addEventListener('click' , () => {
+    
+});
 
-    startBtn.addEventListener( 'click' , () => {
-        player2.style.marginLeft = (parseInt(player2.style.marginLeft)) + gerarNumeroAleatorio() + (px)});
 
-    const player1Win = parseInt(player1.style.marginLeft) > window.innerWidth;
-    const player2Win = parseInt(player2.style.marginLeft) > window.innerWidth;
 
-    if (player1Win){
-        alert('PLAYER 1 VENCEU!!')
-        audioWinner.play();
-    } else{
-        alert('PLAYER 2 VENCEU!!')
-        audioWinner.play();
-    }
-
-    startBtn.addEventListener( 'click' , (evento) => {
-        evento.target.innerText = "Let's Go!"
-    })
-    const cars = document.querySelectorAll('.car');
-    for (let car of cars) {
-        car.addEventListener('click', (event) => {
-
-            const selected = document.querySelector('.selected');
-            if (selected){
-                selected.classList.remove('selected')
-            };
-            event.target.classList.add('selected');
-        });
-    };
-    const personagens = document.querySelectorAll('.playersImages');
-        for (let personagem of personagens){
-            personagem;addEventListener('click', (event) => {
-                const selected = document.querySelector('.selected');
-                if (selected) {
-                    selected.style.backgroundImage = (`url(${event.target.src})`);
-                    
-                };
-                
-            });
-        };
 };
-
-function gerarNumeroAleatorio (){
-    return (Math.random() * 50)
-}
-
-
-
