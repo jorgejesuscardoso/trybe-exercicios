04e4f4d72873118956d5d9c19fed8ec567939a98
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2023-08-02 23:12:31 -0300

    feat: fim da aula 01

--- 02-front-end/1º periodo/1-introducao-ao-frontend-e-javascript-assicrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
+++ 02-front-end/1º periodo/1-introducao-ao-frontend-e-javascript-assicrono/dia-01-ambiente-de-desenvolvimento/gerador-de-senhas/src/main.js
@@ -1,14 +1,35 @@
-import { nanoid } from "nanoid";
-import copy from "clipboard-copy";
+import { nanoid } from 'nanoid';
+import copy from 'clipboard-copy';
+import './style.css';
+
+const main = document.querySelector('main');
 const btnSenha = document.querySelector('button');
 const h2S = document.querySelector('h2');
+// const popup = document.querySelector('main');
 
 btnSenha.addEventListener('click', () => {
-    const nid = nanoid();
-    h2S.innerText = nid;
+  const nid = nanoid();
+  h2S.innerText = nid;
 });
 
 h2S.addEventListener('click', (event) => {
-    copy(event.target.textContent);
-    alert('Senha copiada!')
+  copy(event.target.textContent);
+
+  const popup = document.createElement('div');
+  main.appendChild(popup);
+  popup.id = 'popup';
+
+  const alingCl = document.createElement('div');
+  popup.appendChild(alingCl);
+  alingCl.textContent = 'Senha copiada!';
+  alingCl.id = 'alCl';
+
+  const closePopup = document.createElement('button');
+  closePopup.id = 'clPopup';
+  closePopup.textContent = 'X';
+  popup.appendChild(closePopup);
+
+  closePopup.addEventListener('click', () => {
+    main.removeChild(popup);
+  });
 });
